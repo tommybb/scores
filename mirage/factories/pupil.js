22cbc26cc0398bf904533b25a9b78de3c31411bc
tommybb <t.dudzik@selleo.com>
2017-08-20 13:37:16 +0200

    Apply feedback

--- mirage/factories/pupil.js
+++ mirage/factories/pupil.js
@@ -2,6 +2,8 @@ import { Factory, association } from 'ember-cli-mirage';
 
 export default Factory.extend({
   name: 'Szymon Kolecki',
-  birthdate: new Date(),
+  birthdate: birthdate() {
+               return new Date();
+             },
   classRoom: association(),
 });
